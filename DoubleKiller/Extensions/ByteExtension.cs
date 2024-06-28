11261c763ca7709637ef5c3451c4148e74cd030c
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2024-06-28 11:19:05 +0800

    哈希算法

---
+++ DoubleKiller/Extensions/ByteExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace DoubleKiller
+{
+    internal static class ByteExtension
+    {
+        public static string ToHex(this byte[] data, bool toUpper)
+        {
+            if (data == null || data.Length == 0) return "";
+
+            var format = toUpper ? "X2" : "x2";
+            var result = new StringBuilder(data.Length * 2);
+            foreach (var item in data)
+            {
+                result.Append(item.ToString(format));
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ DoubleKiller/Helpers/HashHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace DoubleKiller
+{
+    internal class HashHelper
+    {
+        public static string Md5(Stream stream, bool toUpper)
+        {
+            return ComputeHash(new MD5CryptoServiceProvider(), stream, toUpper);
+        }
+
+        public static string Sha1(Stream stream, bool toUpper)
+        {
+            return ComputeHash(new SHA1CryptoServiceProvider(), stream, toUpper);
+        }
+
+        public static string Sha256(Stream stream, bool toUpper)
+        {
+            return ComputeHash(new SHA256CryptoServiceProvider(), stream, toUpper);
+        }
+
+        public static string Sha384(Stream stream, bool toUpper)
+        {
+            return ComputeHash(new SHA384CryptoServiceProvider(), stream, toUpper);
+        }
+
+        public static string Sha512(Stream stream, bool toUpper)
+        {
+            return ComputeHash(new SHA512CryptoServiceProvider(), stream, toUpper);
+        }
+
+        private static string ComputeHash(HashAlgorithm hashAlgorithm, Stream stream, bool toUpper)
+        {
+            return hashAlgorithm.ComputeHash(stream).ToHex(toUpper);
+        }
+    }
+}
